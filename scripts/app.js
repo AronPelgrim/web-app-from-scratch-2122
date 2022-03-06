5f2e661fe91730f6c21f12e9a23c3e1199f78239
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-06 11:26:50 +0100

    Added more router stuff

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,40 @@
-import { getPaintings } from './modules/getData.js'
-import { replace } from './modules/getData.js'
-import { dropDown } from './modules/variables.js'
-
+import {
+	getPaintings
+} from './modules/getData.js'
+import {
+	replace
+} from './modules/getData.js'
+import {
+	dropDown
+} from './modules/variables.js'
 
 function onRouteChanged() {
-  const hash = window.location.hash;
-  const li = document.querySelectorAll('li')
+	const hash = window.location.hash;
+	const li = document.querySelectorAll('li')
+	for (let i = 0; i < li.length; i++) {
 
-  for (let i = 0; i < li.length; i++) {
-    if ("#" + (li[i]).id == hash) {
-      
-      const liDetail = document.getElementById(li[i].id)
-      liDetail.classList.add("detail");
+		if ("#" + (li[i]).id == hash) {
 
-        for (const node of document.querySelectorAll("select, option, label, li:not(.detail), li:not(.detail) img, li:not(.detail) p, main h1, a")) {
-    const parent = node.parentNode;
-    const children = Array.from(node.children);
-    for (const child of children) {
-      node.removeChild(child);
-      parent.insertBefore(child, node);
-    }
-    parent.removeChild(node);
-  }
+			const liDetail = document.getElementById(li[i].id)
+			liDetail.classList.add("detail");
 
-      break;
-    }
-  }
+			for (const node of document.querySelectorAll("select, option, label, li:not(.detail), li:not(.detail) img, li:not(.detail) p, main h1, a")) {
+				const parent = node.parentNode;
+				const children = Array.from(node.children);
+				for (const child of children) {
+					node.removeChild(child);
+					parent.insertBefore(child, node);
+				}
+				parent.removeChild(node);
+			}
+			break;
+		} 
+	}
 }
-
 window.addEventListener("hashchange", onRouteChanged);
-
-dropDown.addEventListener("keypress", function (e) {
-    if (e.key === 'Enter') {
-      replace()
-      getPaintings()
-    }
-  });
-
+dropDown.addEventListener("keypress", function(e) {
+	if (e.key === 'Enter') {
+		replace()
+		getPaintings()
+	}
+});
