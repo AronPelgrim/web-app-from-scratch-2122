dce5ff6ca53e687e794d5159e98acd3dd9c12ad3
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-08 10:39:13 +0100

    Some styling

--- scripts/modules/replaceElements.js
+++ scripts/modules/replaceElements.js
@@ -1,5 +1,5 @@
 export function replace(){
-    for (const node of document.querySelectorAll("li, img, p, main h1, button, a")) {
+    for (const node of document.querySelectorAll("li, img, p, main h1, button, main a")) {
       const parent = node.parentNode;
       const children = Array.from(node.children);
       for (const child of children) {
